908947f2e3528da45e971b8b83d28c5cd77e7b01
abdenasser <nasser.elidrissi065@gmail.com>
2016-12-07 21:56:42 +0000

    using autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ const gulp = require('gulp');
 const postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
 
+var autoprefixer = require('autoprefixer');
+
 gulp.task('css', function() {
   gulp.src('css/app.css')
-    .pipe(postcss([]))
+    .pipe(postcss([ autoprefixer({ browsers: ['last 5 versions'] }) ]))
     .pipe(rename('bundle.css'))
     .pipe(gulp.dest('css/'))
 })
